97a9fd394b1b92fe93a85c59ba3e3abe918df810
unknown <juandaesaputra@gmail.com>
2022-11-14 17:41:14 +0700

    darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Epilogue } from "@next/font/google";
+import { ThemeProvider } from "next-themes";
+
+const epilogue = Epilogue();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <main className={epilogue.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { ThemeToggle } from '../components/theme'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,6 +13,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <ThemeToggle />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
