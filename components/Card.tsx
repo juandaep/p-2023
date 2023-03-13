71041be541b164b4884126abfc6349fc240b1dd7
unknown <juandaesaputra@gmail.com>
2023-03-13 15:24:50 +0700

    Add title on mixing card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,6 @@
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import React, { FC } from "react";
+import ReactPlayer from "react-player";
 import { classNames } from "utils/classnames";
 
 interface CardProps {
@@ -29,7 +30,7 @@ const Card: FC<CardProps> = ({
         <img className="w-auto rounded-lg shadow-xl" src={img} />
       </div>
       <div
-        className={classNames("flex flex-col flex-auto p-4 gap-1", "md:p-6")}
+        className={classNames("flex flex-col flex-auto p-2")}
       >
         <h4
           className={classNames(
@@ -63,4 +64,44 @@ const Card: FC<CardProps> = ({
   );
 };
 
-export default Card;
+interface CardMixingProps {
+  url: string;
+  title: string;
+  description: string;
+}
+const MixingCard: FC<CardMixingProps> = ({ url, title, description }) => {
+  return (
+    <div
+      className={classNames(
+        "flex flex-col rounded-lg bg-white shadow-md border box-border border-neutral-200 mb-6",
+        "dark:bg-neutral-800 dark:border dark:box-border dark:border-neutral-600"
+      )}
+    >
+      <div className={classNames("flex flex-col gap-4 p-4 py-4")}>
+        <ReactPlayer url={url} width="100%" height="100%" />
+        <div
+          className={classNames("flex flex-col flex-auto gap-1", "md:p-6")}
+        >
+          <h4
+            className={classNames(
+              "text-xl font-medium text-neutral-900",
+              "dark:text-white"
+            )}
+          >
+            {title}
+          </h4>
+          <p
+            className={classNames(
+              "text-neutral-600 text-base",
+              "dark:text-neutral-400"
+            )}
+          >
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Card, MixingCard };
--- components/Tabs/TabDesign.tsx
+++ components/Tabs/TabDesign.tsx
@@ -1,5 +1,5 @@
 import { FC, Fragment } from "react";
-import Card from "../Card";
+import { Card } from "../Card";
 
 const TabDesign: FC<{}> = () => {
   return (
