d28d6fd6a680f3e60ab5a986f5c5895fbb1259fb
unknown <juandaesaputra@gmail.com>
2023-03-13 15:40:41 +0700

    fixing card components.

--- components/Tabs/TabMixing.tsx
+++ components/Tabs/TabMixing.tsx
@@ -7,7 +7,7 @@ const TabMixing: FC<{}> = () => {
       <MixingCard
             url="https://soundcloud.com/juanda-ep/paramore-thats-what-you-get-mix-cover?si=c4e2d5f5ea2b42b7bdde0a03b1e99fc5&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
             title="Paramore - Thats What You Get"
-            description="This is a testing for card components"
+            description="This is a testing for card components."
           />
     </Fragment>
   );
